a730530f931e2a0bea561adc60253cbd6a1e3201
sita-chittula <info@joindevops.com>
2025-09-21 21:40:08 +0530

    variables

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh::Hi suresh,how are you?"
+echo "Suresh::hello Ramesh ,I am fine.How are you?"
+echo "Ramesh::what are you learning recently?"
+echo "Suresh::Iam learning Shell scripting Ramesh"
---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1::Hi $PERSON2,how are you?"
+echo "$PERSON2::hello $PERSON1 ,I am fine.How are you?"
+echo "$PERSON1::what are you learning recently?"
+echo "$PERSON2::Iam learning Shell scripting $PERSON1"
