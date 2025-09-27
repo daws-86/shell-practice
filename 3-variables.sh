baeafe6f0fefcafafe44e3575f48c5070b088b2c
sita-chittula <info@joindevops.com>
2025-09-27 23:17:27 +0530

    variables

---
+++ 3-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Welcome to Alaska $PERSON2"
+echo "$PERSON2:: Thank you Trump,Finally not on Zoom Webiner"
+echo "$PERSON1:: So, do we have a deal"
+echo "$PERSON2:: Yes, if you make Ukraine joins Russia"
