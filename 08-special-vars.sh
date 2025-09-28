a0365249e65925c2e9d4add15f62ba3b8bb7fce6
sita-chittula <info@joindevops.com>
2025-09-28 10:19:20 +0530

    variables

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -4,3 +4,6 @@ echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
 
 echo "script name: $0"
+echo "current working direcory: $pwd"
+echo "Who is running: $USER"
+echo "Home directory of user: $Home"
