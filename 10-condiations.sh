c228bb829eacd9eaa4d6a631ed2041edad09bf25
sita-chittula <info@joindevops.com>
2025-09-28 21:32:53 +0530

    variables

--- 10-condiations.sh
+++ 10-condiations.sh
@@ -2,13 +2,10 @@
 
 NUMBER=$1
 
-if [ $NUMBER - lt 10]; then
-
-echo "if the Given $NUMBER is less than 10"
-
+if [ $NUMBER -lt 10 ]; then
+   echo "if the Given $NUMBER is less than 10"
 else
- echo "if the the Given $NUMBER is greater than or equal to 10"
-
+    echo "if the the Given $NUMBER is greater than or equal to 10"
  fi
  
  #-gt
