152153681036fb576c855a29246ca89b7f154713
sita-chittula <info@joindevops.com>
2025-09-28 13:23:34 +0530

    variables

---  09-data-types.sh
+++  09-data-types.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-#everything in shell considered as string
+# everything in shell considered as string
 
 NUMBER1=100
 NUMBER2=200
 NAME=Devops
 
-SUM=$(($NUMBER1+$NUMBER2+$NAME))
+SUM=$(($NUMBER1 + $NUMBER2))
 
 echo "sum is: ${SUM}"
+echo "Name is: ${NAME}"
 
-LEADERS=("putin" "modi" "truph" "Trudo")
+LEADERS=("putin" "modi" "trump" "trudeau")
 
 echo "All leaders: ${LEADERS[@]}"
