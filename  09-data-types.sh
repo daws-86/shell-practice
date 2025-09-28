8cc611d9296a0005f84b8b73f5a95c287a53cd41
sita-chittula <info@joindevops.com>
2025-09-28 18:39:34 +0530

    variables

---  09-data-types.sh
+++  09-data-types.sh
@@ -11,6 +11,10 @@ SUM=$(($NUMBER1 + $NUMBER2))
 echo "sum is: ${SUM}"
 echo "Name is: ${NAME}"
 
+# size= 4  Max index = 3
+
 LEADERS=("putin" "modi" "trump" "trudeau")
 
 echo "All leaders: ${LEADERS[@]}"
+echo "First Leader: ${Leader[0]}"
+echo "First Leader: ${Leader[10]}"
---
+++ 10-condiations.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER - lt 10]; then
+
+echo "if the Given $NUMBER is less than 10"
+
+else
+ echo "if the the Given $NUMBER is greater than or equal to 10"
+
+ fi
+ 
+ #-gt
+ #-ne
+ #-eq
