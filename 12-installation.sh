79ae14d0c812b2359733ab1d64776f9a3e8ea999
sita-chittula <info@joindevops.com>
2025-09-28 22:30:05 +0530

    variables

--- 12-installation.sh
+++ 12-installation.sh
@@ -4,11 +4,13 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run this script with root privilege"
+    exit 1 # failure is other than 0
 fi
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
    echo "ERROR:: Installing MYSQL is failure"
+   exit 1
 else
     echo "Installing MYSQL is SUCCESS"
 fi   
