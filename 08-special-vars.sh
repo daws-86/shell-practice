9f5998313ffa03aa7d07fe649fcc2ed871a1afec
sita-chittula <info@joindevops.com>
2025-09-28 10:29:14 +0530

    variables

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -7,3 +7,6 @@ echo "script name: $0"
 echo "current working direcory: $PWD"
 echo "Who is running: $USER"
 echo "Home directory of user: $HOME"
+echo "PID of the script: $$"
+sleep 50 &
+echo "PID of the last command in the back ground is: $!"
